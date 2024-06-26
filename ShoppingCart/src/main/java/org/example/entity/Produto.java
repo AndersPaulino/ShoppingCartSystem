0e0af7d73536bfe2e77454d253ae41f48bf6c77f
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2024-06-25 21:26:06 -0300

    Criação dos Getters, Setters e Constructors da classe Produto e criação do metodo calcularPrecoVenda.

--- ShoppingCart/src/main/java/org/example/entity/Produto.java
+++ ShoppingCart/src/main/java/org/example/entity/Produto.java
@@ -1,4 +1,91 @@
 package org.example.entity;
 
 public class Produto {
+    private int codigo;
+    private String descricao;
+    private String categoria;
+    private double precoCompra;
+    private double margemLucro;
+    private double precoVenda;
+    private int quantidadeEstoque;
+    private int estoqueMinimo;
+
+    public Produto(int codigo, String descricao, String categoria, double precoCompra, double margemLucro, int quantidadeEstoque, int estoqueMinimo) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.precoCompra = precoCompra;
+        this.margemLucro = margemLucro;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.estoqueMinimo = estoqueMinimo;
+        calcularPrecoVenda();
+    }
+
+    public void calcularPrecoVenda() {
+        this.precoVenda = this.precoCompra * (1 + this.margemLucro / 100);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(double precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public double getMargemLucro() {
+        return margemLucro;
+    }
+
+    public void setMargemLucro(double margemLucro) {
+        this.margemLucro = margemLucro;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public int getEstoqueMinimo() {
+        return estoqueMinimo;
+    }
+
+    public void setEstoqueMinimo(int estoqueMinimo) {
+        this.estoqueMinimo = estoqueMinimo;
+    }
 }
