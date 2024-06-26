029154f53f83003904f65276dd08b2532e4adf45
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2024-06-25 21:26:54 -0300

    Criação dos Getters, Setters e Constructors da classe Item.

--- ShoppingCart/src/main/java/org/example/entity/Item.java
+++ ShoppingCart/src/main/java/org/example/entity/Item.java
@@ -1,4 +1,46 @@
 package org.example.entity;
 
 public class Item {
+    private Produto produto;
+    private int quantidade;
+    private double valorUnitario;
+    private double valorTotal;
+    public Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.valorUnitario = produto.getPrecoVenda();
+        this.valorTotal = this.quantidade * this.valorUnitario;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
